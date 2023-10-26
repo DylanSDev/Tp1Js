be89da56ab86be9c28819cfa3f06903c5848f7c6
DylanSDev <dylansdiaz.dev@gmail.com>
2023-10-26 00:09:43 -0300

    Se ha desarrollado el codigo del Ejercicio N6

---
+++ js/ejercicio6.js
@@ -0,0 +1,6 @@
+
+let num1 = parseInt(prompt("Ingrese el primer número: "));
+let num2 = parseInt(prompt("Ingrese el segundo número: "));
+
+if(num1>=num2)document.write("El " + num1 + " es el número más grande ingresado.");
+if(num2>num1)document.write("El " + num2 + " es el número más grande ingresado.");
