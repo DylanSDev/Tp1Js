92d7be0bb14ea2e1ac346996d52672a3bc8e822a
DylanSDev <dylansdiaz.dev@gmail.com>
2023-10-26 15:16:56 -0300

    Se ha corregido el ejercicio 9

--- js/ejercicio9.js
+++ js/ejercicio9.js
@@ -1,5 +1,8 @@
 let frase = prompt("Ingrese una frase: ");
+frase = frase.toLowerCase();
 let vocales = ["a","e","i","o","u"];
+
+document.write("Las vocales que aparecen en la frase son: ");
 for(let i=0; i < frase.length; i++)
 {
    if(vocales.includes(frase.substring(i, i+1))) document.write(frase.substring(i, i+1));
